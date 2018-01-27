a054596206d83ad342b9553c7cba9f3c2ed03e86
Landon Shields <landonwshields@gmail.com>
2018-01-27 14:56:49 -0700

    changed forms

--- seeds/01_forms.js
+++ seeds/01_forms.js
@@ -5,13 +5,21 @@
       .then(function () {
         // Inserts seed entries
         return knex('forms').insert([
-          { 
-            registrationForm: 'https://docs.wixstatic.com/ugd/8f167c_408bbda2f26a4999b7ed3d406bc82e6f.pdf',
-            emergencyContactForm: 'https://docs.wixstatic.com/ugd/8f167c_59937af39fbc40fb912417a138547f77.pdf',
-            emergencyMedicalConsent: 'https://docs.wixstatic.com/ugd/8f167c_14e60c2eca9f4ebabef244c7ece83a72.pdf',
-            generalHealthForm: 'https://docs.wixstatic.com/ugd/8f167c_45507647fefb4ea79abfd2717f795c12.pdf',
-            immunizationForm: 'https://docs.wixstatic.com/ugd/8f167c_b53a363ff6324e12bf9978b537689ecf.pdf'
-           }
+          {
+            schoolForm: 'https://docs.wixstatic.com/ugd/8f167c_408bbda2f26a4999b7ed3d406bc82e6f.pdf'
+          },
+          {
+            schoolForm: 'https://docs.wixstatic.com/ugd/8f167c_59937af39fbc40fb912417a138547f77.pdf'
+          },
+          {
+            schoolForm: 'https://docs.wixstatic.com/ugd/8f167c_14e60c2eca9f4ebabef244c7ece83a72.pdf'
+          },
+          {
+            schoolForm: 'https://docs.wixstatic.com/ugd/8f167c_45507647fefb4ea79abfd2717f795c12.pdf'
+          },
+          {
+            schoolForm: 'https://docs.wixstatic.com/ugd/8f167c_b53a363ff6324e12bf9978b537689ecf.pdf'
+          }
         ]);
       });
   };
